7b5198c224b2bb85cea10cd4ef5701d0b4551205
akaziki <874601941@qq.com>
2024-05-28 13:24:05 +0800

    使用element plus的container实现基本的页面展示

--- wms-web/src/main.js
+++ wms-web/src/main.js
@@ -1,10 +1,6 @@
-import './assets/main.css'
-
 import {createApp} from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import './assets/global.css'
 
 const app = createApp(App)
-app.use(ElementPlus)
 app.mount('#app')
